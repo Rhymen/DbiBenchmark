fa56b8dd8204d05c2ce65334ac2b080e0edd618f
Lucas Engelke <L-Engelke@web.de>
2017-11-30 11:12:22 +0100

    implements benchmarking

--- src/DbiBenchmark.java
+++ src/DbiBenchmark.java
@@ -1,10 +1,16 @@
 import db.BenchmarkDB;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class DbiBenchmark {
     public static void main(String[] args) {
         try {
             BenchmarkDB db = new BenchmarkDB();
+            Instant starts = Instant.now();
             db.createDatabase(10);
+            Instant ends = Instant.now();
+            System.out.println(Duration.between(starts, ends));
         } catch(Exception ex) {
             ex.printStackTrace();
         }
